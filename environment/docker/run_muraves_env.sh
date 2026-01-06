a57ac6f6fd34f3267fbe6af8d869fb9af68f0f7c
muravesusers user <abiolchi@m7.iihe.ac.be>
2026-01-06 16:43:12 +0100

    simplify users commands

--- environment/docker/run_muraves_env.sh
+++ environment/docker/run_muraves_env.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 # -----------------------------
 # Wrapper script to run muraves-env.sif on T2B
 # -----------------------------
@@ -8,12 +9,47 @@
 # - automatically activate the mamba environment 
 # - Check existance of Git repository in the binded worspace
 
+echo "=== Muraves environment setup ==="
+
+# Ask user for workspace location
+read -rp "Enter workspace location [default: \$HOME]: " WORKSPACE_INPUT
+
+# Use HOME if nothing is provided
+if [ -z "$WORKSPACE_INPUT" ]; then
+    WORKSPACE_HOST="$HOME"
+else
+    # Expand ~ if used
+    WORKSPACE_HOST="$(eval echo "$WORKSPACE_INPUT")"
+fi
+
+export WORKSPACE_HOST
+echo "Using workspace: $WORKSPACE_HOST"
+# Create workspace if it doesn't exist
+mkdir -p "$WORKSPACE_HOST"
+
+# Clone repository if not present
+REPO_URL="https://github.com/muraves/Software.git"
+WORKSPACE_REPO="$WORKSPACE_HOST/Software"
+if [ ! -d "$WORKSPACE_REPO/Software" ]; then
+    echo "Cloning Muraves repository..."
+    git clone "$REPO_URL" "$WORKSPACE_REPO"
+else
+    echo "Repository already exists: $WORKSPACE_REPO"
+fi
+
+# Move into repo (important if script is run from elsewhere)
+cd "$WORKSPACE_REPO"
+
+
+# Ensure scripts are executable
+chmod +x environment/docker/entrypoint.sh
+chmod +x environment/docker/run_muraves_env.sh
 
 # Path to your Singularity image
 SIF_PATH="/group/Muography/MURAVES/container/muraves-env.sif"
 
 # Bind your workspace (host repo directory)
-WORKSPACE_HOST="path/to/your/git/repo"
+#WORKSPACE_HOST="path/to/your/git/repo"
 WORKSPACE_CONTAINER="/workspace"
 
 DIR_HOST="/pnfs/iihe/muraves/muraves_DATA"
